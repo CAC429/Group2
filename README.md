521ea61db637f20e302d0eaf5b9d22b489988c98
Ethan Nam <etn13@pitt.edu>
2025-04-04 16:15:37 -0400

    blah blah blah

--- README.md
+++
@@ -1,4 +0,0 @@
-# Group2
-This is the github repository for all of the code that will be created and utilized by 1140 trains group 2
-
-this is my first branch, ETHAN NAM!
--- main_TCHW.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-tc_hw_process = subprocess.Popen(["python", "train_controller_HW.py"])
-tm_process = subprocess.Popen(["python", "Main_Train_Model.py"])
-
-tc_hw_process.wait()
-tm_process.wait()
