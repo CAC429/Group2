9a9efb5d15eb0aec81827772375c721297ff264e
Philip <pls35@pitt.edu>
2025-04-22 15:10:08 -0400

    clean up

--- TCTM_combined.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-tc_process = subprocess.Popen(["python", "uiscript.py"])
-tm_process = subprocess.Popen(["python", "Main_Train_Model.py"])
-
-tc_process.wait()
-tm_process.wait()
